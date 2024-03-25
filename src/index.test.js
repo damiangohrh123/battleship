de43acf075630873a318f8072d0d395843faa1a7
damiangohrh123 <damiangohrh@gmail.com>
2024-03-25 14:21:13 +0800

    Add test for gameboard creation

--- src/index.test.js
+++ src/index.test.js
@@ -6,7 +6,7 @@ describe("Ship", () => {
 
   beforeEach(() => {
     ship = new index.Ship(3);
-  })
+  });
 
   test("Ship creation", () => {
     expect(ship.length).toBe(3);
@@ -15,5 +15,19 @@ describe("Ship", () => {
 
   test("Ship isSunk", () => {
     expect(ship.isSunk()).toBe(false);
-  })
+  });
+})
+
+describe("Gameboard", () => {
+  let board;
+
+  beforeEach(() => {
+    board = new Gameboard();
+  });
+
+  test("Gameboard creation", () => {
+    // Check the rows and columms of the gameboad
+    expect(board.grid.length).toBe(10);
+    expect(board.grid[0].length).toBe(10);
+  });
 })
