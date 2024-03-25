9d177792265dcbd5d7256f15f25eb1bc237f75c0
damiangohrh123 <damiangohrh@gmail.com>
2024-03-25 20:54:30 +0800

    Refactor tests

--- src/index.test.js
+++ src/index.test.js
@@ -2,18 +2,11 @@ const index = require("./index");
 
 
 describe("Ship", () => {
-  let ship;
-
-  beforeEach(() => {
-    ship = new index.Ship(3, "submarine");
-  });
+  const ship = new index.Ship(3, "submarine");
 
   test("Ship creation", () => {
     expect(ship.length).toBe(3);
     expect(ship.hits).toBe(0);
-  });
-
-  test("Ship isSunk", () => {
     expect(ship.isSunk()).toBe(false);
   });
 })
@@ -25,57 +18,62 @@ describe("Gameboard", () => {
     board = new index.Gameboard(10, 10);
   });
 
-  test("Gameboard creation with correct dimensions (10 x 10)", () => {
-    // Check if grid has correct dimensions
-    expect(board.grid.length).toBe(10);
-    board.grid.forEach(row => {
-      expect(row.length).toBe(10);
-    })
+  describe("Gameboard creation", () => {
 
-    // Check if each cell contains the expected coordinates
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
-        expect(board.grid[i][j]).toEqual({ x: i, y: j });
+    test("Gameboard creation with correct dimensions (10 x 10)", () => {
+      // Check if grid has correct dimensions
+      expect(board.grid.length).toBe(10);
+      board.grid.forEach(row => {
+        expect(row.length).toBe(10);
+      })
+  
+      // Check if each cell contains the expected coordinates
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          expect(board.grid[i][j]).toEqual({ x: i, y: j });
+        }
       }
-    }
+    });
   });
 
-  test("Placing ships horizontally on board", () => {
-    const ship = new index.Ship(3, "submarine");
-    const position = { x: 2, y: 3};
-    const orientation = "horizontal";
-    
-    board.placeShip(ship, position, orientation);
+  describe("Placing ship", () => {
+    let ship;
 
-    // Verify if ship is placed at correct coordinates and orientation
-    expect(board.grid[position.x][position.y]).toBe(ship);
-    expect(board.grid[position.x][position.y + 1]).toBe(ship);
-    expect(board.grid[position.x][position.y + 2]).toBe(ship);
+    beforeEach(() => {
+      ship = new index.Ship(3, "submarine");
+    });
+
+    test("Placing ships horizontally & vertically on board", () => {
+      board.placeShip(ship, {x: 2, y: 3}, "horizontal");
+  
+      // Verify if ship is placed at correct coordinates and orientation
+      expect(board.grid[2][3]).toBe(ship);
+      expect(board.grid[2][3 + 1]).toBe(ship);
+      expect(board.grid[2][3 + 2]).toBe(ship);
+
+      board.placeShip(ship, {x: 2, y: 3}, "vertical");
+  
+      // Verify if ship is placed at correct coordinates and orientation
+      expect(board.grid[2][3]).toBe(ship);
+      expect(board.grid[2 + 1][3]).toBe(ship);
+      expect(board.grid[2 + 2][3]).toBe(ship);
+    });
+  
+    test("Placing ships out of bounds", () => {
+      // Try placing ship out of bounds initally
+      try {
+        board.placeShip(ship, {x: 10, y: 10}, "horizontal");
+      } catch(error) {
+        expect(error.message).toBe("Ship is out of bounds.");
+      }
+
+      // Try placing ship in bounds initially, but the length of ship will be out of bounds
+      try {
+        board.placeShip(ship, {x: 8, y: 8}, "horizontal");
+      } catch(error) {
+        expect(error.message).toBe("Ship is out of bounds.")
+      }
+    });
   });
 
-  test("Placing ships vertically on board", () => {
-    const ship = new index.Ship(3, "submarine");
-    const position = { x: 2, y: 3};
-    const orientation = "vertical";
-
-    board.placeShip(ship, position, orientation);
-
-    // Verify if ship is placed at correct coordinates and orientation
-    expect(board.grid[position.x][position.y]).toBe(ship);
-    expect(board.grid[position.x + 1][position.y]).toBe(ship);
-    expect(board.grid[position.x + 2][position.y]).toBe(ship);
-  });
-
-  test("Placing ships out of bounds", () => {
-    const ship = new index.Ship(3, "submarine");
-    const position = { x: 10, y: 10};
-    const orientation = "horizontal";
-
-    // Verify if ship is not placed
-    try {
-      board.placeShip(ship, position, orientation);
-    } catch(error) {
-      expect(error.message).toBe("Ship is out of bounds.")
-    }
-  });
 })
